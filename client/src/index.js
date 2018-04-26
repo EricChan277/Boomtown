b911ab68d2991f060686995259734ee937574243
Eric Chan <ericchan277@gmail.com>
2018-04-26 14:34:47 -0700

    Added CORS

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
 
 import registerServiceWorker from './registerServiceWorker';
 import store from './redux/store';
@@ -11,15 +12,19 @@ import muiTheme from './config/theme';
 import Layout from './components/Layout';
 import Routes from './Routes';
 
+import client from './config/apolloClient';
+
 const Boomtown = () => (
     <MuiThemeProvider muiTheme={muiTheme}>
-        <Provider store={store}>
-            <Router>
-                <Layout>
-                    <Routes />
-                </Layout>
-            </Router>
-        </Provider>
+        <ApolloProvider client={client}>
+            <Provider store={store}>
+                <Router>
+                    <Layout>
+                        <Routes />
+                    </Layout>
+                </Router>
+            </Provider>
+        </ApolloProvider>
     </MuiThemeProvider>
 );
 
--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-
+import cors from 'cors';
 import schema from './schema';
 
 const app = express();
 const PORT = 3333;
 
+app.use('*', cors());
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
 app.listen(PORT, err => {
   if (err) {
     console.log('ERROR starting Express ...', err);
   } else {
-    console.log(`Express GraphQL server is running. Access GraphiQL on: http://localhost:${PORT}/graphiql`);
+    console.log(
+      `Express GraphQL server is running. Access GraphiQL on: http://localhost:${PORT}/graphiql`
+    );
   }
 });
 
